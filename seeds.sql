664b23cc1e97c314b430b14e527c67c6a92cefbe
Francisco Rones <ininorones@gmail.com>
2021-01-08 22:48:17 -0800

    Update: Seeds updated to re-arrange tables to Department, Role and then Employee last.

--- seeds.sql
+++ seeds.sql
@@ -4,13 +4,10 @@ CREATE DATABASE employee_tracker_db;
 
 USE employee_tracker_db;
 
-CREATE TABLE employee (
-  id int NOT NULL AUTO_INCREMENT,
-  first_name varchar(30) NOT NULL,
-  last_name varchar(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  PRIMARY KEY (id)
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
@@ -18,13 +15,22 @@ CREATE TABLE role (
 	title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department (id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE department (
-	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NULL,
-    PRIMARY KEY (id)
+CREATE TABLE employee (
+  id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES role (id)
+  ON DELETE CASCADE
 );
 
 -- pre-made values for testing
