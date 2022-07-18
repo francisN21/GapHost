0daf5d57dbee6e5c7656d01fcbd1845d3580137d
Francisco Rones - Admin <franciscoadmin@security1stks.com>
2022-07-18 16:46:02 -0700

    Updating and Uploading seeds.sql

---
+++ tftInvite.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS tftInvite_db;
+
+CREATE DATABASE tftInvite_db;
+
+USE tftInvite_db;
+
+CREATE TABLE Users (
+	user_id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id),
+	UNIQUE KEY email (email),
+	UNIQUE KEY username (username)
+);
+
+CREATE TABLE Posts (
+	post_id INT NOT NULL AUTO_INCREMENT,
+	user_id INT NOT NULL,
+    caption VARCHAR(255) NOT NULL,
+    PRIMARY KEY (post_id),
+    FOREIGN KEY (`user_id`) REFERENCES Users (`user_id`)
+);
+
+CREATE TABLE invite (
+  invite_id int NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  content VARCHAR(255) NULL,
+  address VARCHAR(255) NULL,
+  target_date DATE NULL,
+  attribute1 VARCHAR(255) NULL,
+  attribute2 VARCHAR(255) NULL,
+  attribute3 VARCHAR(255) NULL,
+  date_created DATE NOT NULL,
+  date_updated DATE,
+  PRIMARY KEY (invite_id),
+  FOREIGN KEY (`user_id`) REFERENCES Users (`user_id`)
+);
